25f7b87db839e6eeb0691b8e9a8429780b9384f3
livehass <silva.felipe12@hotmail.com>
2025-02-25 11:30:37 -0300

    added initial files to the branch main, at the start of the project

---
+++ src/test/java/apirest/dio/Api/projeto/da/dio/ApiProjetoDaDioApplicationTests.java
@@ -0,0 +1,13 @@
+package apirest.dio.Api.projeto.da.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjetoDaDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
