6aad78f4d2508e46177ebaf15f67e4ea4d049efd
livehass <silva.felipe12@hotmail.com>
2025-02-26 20:31:40 -0300

    added description to the project

--- src/main/java/dio/api_rest/config/OpenApiConfig.java
+++ src/main/java/dio/api_rest/config/OpenApiConfig.java
@@ -12,8 +12,8 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API REST Example")
+                        .title("API RESTful Dio bootcamp Avanade created by Jorge Felipe")
                         .version("1.0")
-                        .description("API REST example using Spring Boot"));
+                        .description("This project is a RESTful API developed in Java using the Spring Boot framework. The API is configured to use Springdoc OpenAPI to generate endpoint documentation in Swagger UI. The project is managed with Gradle"));
     }
 }
